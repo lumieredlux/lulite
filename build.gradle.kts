5f1a1b35eaf5fa575b05991c8c41f7875c17126b
lumieredlux <168293399+lumieredlux@users.noreply.github.com>
2025-08-27 16:10:56 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("com.android.application") version "8.5.1" apply false
-  kotlin("android") version libs.versions.kotlin.get() apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
 }
